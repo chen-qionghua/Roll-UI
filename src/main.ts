306330a8ceab9bf98a9d28010d38db5c053ee642
chengqionghua <1169121469@qq.com>
2022-01-10 17:31:35 +0800

    Topnav组件封装完成

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
-    history: history,
-    routes: [
-        { path: '/', component: Home },
-        { path: '/doc', component: Doc }
+  history: history,
+  routes: [
+    { path: '/', component: Home },
+    { path: '/doc', component: Doc }
 
-    ]
+  ]
 })
 
 const app = createApp(App)
