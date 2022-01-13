6026569bd150cd6798ad48682becf5dd8bdef419
chengqionghua <1169121469@qq.com>
2022-01-13 16:52:14 +0800

    Button添加theme属性

--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -1,3 +1,14 @@
 <template>
-  <button><slot></button>
+  <button class="roll-ui" :class="`theme-${theme}`"><slot /></button>
 </template>
+<script lang="ts">
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+  },
+  setup() {},
+};
+</script>
