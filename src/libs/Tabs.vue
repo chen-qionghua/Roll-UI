353ef7cbaa9f1dc85a56e49069b1d16493a40314
chengqionghua <1169121469@qq.com>
2022-01-19 02:18:09 +0800

    渲染Tabs嵌套组件

--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>Tabs组件</div>
+  <div>
+    Tabs组件
+    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    <component v-for="(c, index) in defaults" :is="c" :key="index"></component>
+  </div>
 </template>
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  components: { Tab },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.map((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs子标签必须是Tab");
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return { defaults, titles };
+  },
+};
+</script>
