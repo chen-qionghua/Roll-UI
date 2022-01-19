93459cf2bd1c3583eb6cfc56dc22243af64baba6
chengqionghua <1169121469@qq.com>
2022-01-19 11:00:56 +0800

    Tabs组件的CSS1

--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    Tabs组件
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index"></component>
+  <div class="roll-tabs">
+    <div class="roll-tabs-nav">
+      <div class="roll-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="roll-tabs-content">
+      <component
+        class="roll-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      ></component>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -23,3 +33,32 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.roll-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
