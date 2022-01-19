d618d415807104c74019d5c0d3246432791a7f86
chengqionghua <1169121469@qq.com>
2022-01-19 17:14:33 +0800

    完成Tabs组件的下划线设计宽度

--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -10,6 +10,7 @@
       >
         {{ t }}
       </div>
+      <div class="roll-tabs-nav-indicator"></div>
     </div>
     <div class="roll-tabs-content">
       <component
@@ -63,12 +64,12 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
-
 .roll-tabs {
   &-nav {
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -81,6 +82,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
   &-content {
