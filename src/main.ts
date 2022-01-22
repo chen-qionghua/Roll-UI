9a570d8fcdf3f7b404cdc22ec527b918f8b3d947
chengqionghua <1169121469@qq.com>
2022-01-22 14:48:26 +0800

    抽离出Markdown组件优化代码

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './index.scss'
 import './libs/roll.scss'
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
-
+import Markdown from './components/Markdown.vue'
 import { router } from './router'
 import "github-markdown-css"
 
@@ -10,3 +10,4 @@ import "github-markdown-css"
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+app.component('Markdown', Markdown)
