2ace99e3aee0d9fd4beed29a64a9242ba91a1620
chengqionghua <1169121469@qq.com>
2022-01-23 02:30:33 +0800

    SwitchDemo删除无用代码优化

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -6,20 +6,14 @@
   </div>
 </template>
 <script lang="ts">
-import { ref, h } from "vue";
-import Button from "../libs/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
 import Demo from "./Demo.vue";
-import "prismjs";
-import "prismjs/themes/prism-okaidia.min.css";
-const Prism = (window as any).Prism;
 
 export default {
-  components: { Button, Demo },
+  components: { Demo },
   setup() {
-    const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo, Prism };
+    return { Switch1Demo, Switch2Demo };
   },
 };
 </script>
